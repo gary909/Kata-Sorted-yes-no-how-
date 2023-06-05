e80799fa214f09fb125d367f66fb3202cab5e0bb
gary909 <garywhite61@hotmail.com>
2023-06-05 12:52:03 +0200

    code

---
+++ app.js
@@ -0,0 +1,6 @@
+function isSortedAndHow(array) {
+}
+
+console.log(isSortedAndHow([1, 2])); // 'yes, ascending'
+console.log(isSortedAndHow([15, 7, 3, -8])); // 'yes, descending'
+console.log(isSortedAndHow([4, 2, 30])); // 'no'
